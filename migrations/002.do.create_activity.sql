4e1d63640e884fb45b7364fc20da63a609d27bfd
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-31 10:52:11 -0600

    new wonderful tables git add .

--- migrations/002.do.create_activity.sql
+++ migrations/002.do.create_activity.sql
@@ -7,5 +7,7 @@ CREATE TABLE activity(
      */
      is_accepted BOOL DEFAULT '0',
      is_rejected BOOL DEFAULT '0' ,
-     date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+     date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     creator_id INTEGER REFERENCES app_user(id)
+
 );
---
+++ migrations/003.undo.create_accepted_rejected.sql
@@ -0,0 +1,4 @@
+ALTER TABLE activity
+    DROP COLUMN IF  EXISTS category_id;
+
+DROP TABLE IF EXISTS accepted_rejected;
