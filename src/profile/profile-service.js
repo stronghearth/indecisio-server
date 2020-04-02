f74f4cbaadfd12b09f13dbcd1a49cd7a30f9ddb8
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-04-02 14:09:55 -0400

    teeny merge conflict resolved

--- src/profile/profile-service.js
+++ src/profile/profile-service.js
@@ -1,35 +1,5 @@
 const ProfileService = {
 
-<<<<<<< HEAD
-  getTopActivitiesList(db){
-    return db
-      .select('name', 'accepted_count')
-      .from('activity')
-      .orderBy([{column: 'accepted_count', order: 'desc'}, {column: 'name'}]);
-  },
-
-  getUserTopActivitiesList(db, userId){
-    return db   
-      .select('accepted_count')
-      .from('accepted_rejected')
-      .where({user_id: userId})
-      .orderBy('accepted_count', 'desc');
-  },
-  insertAcceptedRejectedRow(db, newRow) {
-    return db
-      .insert(newRow)
-      .into('accepted_rejected')
-      .returning('*')
-      .then(([row]) => row);
-  },
-
-  updateAcceptedCount(db, activityId, userId, acceptedCount){
-    return db('accepted_rejected')
-      .where({activity: activityId, user_id: userId})
-      .first()
-      .update({accepted_count: acceptedCount});
-  },
-=======
     getTopActivitiesList(db){
         return db
                 .select('name', 'global_accepted_count')
@@ -72,7 +42,6 @@ const ProfileService = {
                     return rows[0];
                   });
     },
->>>>>>> 3d1db9525d9dc08c5c0ce031476ad84308923fc9
  
 };
 
