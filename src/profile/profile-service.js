dd6042e0f5fa83dfc6569c919467f506ab863b36
Blade Boles <bladerboles@gmail.com>
2020-04-02 12:13:27 -0600

    Dang eslint

--- src/profile/profile-service.js
+++ src/profile/profile-service.js
@@ -1,47 +1,47 @@
 const ProfileService = {
 
-    getTopActivitiesList(db){
-        return db
-                .select('name', 'global_accepted_count')
-                .from('activity')
-                .orderBy([{column: 'global_accepted_count', order: 'desc'}, {column: 'name'}])
-                .limit(10)
-    },
+  getTopActivitiesList(db){
+    return db
+      .select('name', 'global_accepted_count')
+      .from('activity')
+      .orderBy([{column: 'global_accepted_count', order: 'desc'}, {column: 'name'}])
+      .limit(10);
+  },
 
-    getUserTopActivitiesList(db, userId){
-        return db   
-                .select('name', 'accepted_count')
-                .from('accepted_rejected')
-                .rightJoin('activity', 'activity', 'activity.id')
-                .where({user_id: userId})
-                .orderBy([{column: 'accepted_count', order: 'desc'}, {column: 'name'}])
-                .limit(10)
-    },
+  getUserTopActivitiesList(db, userId){
+    return db   
+      .select('name', 'accepted_count')
+      .from('accepted_rejected')
+      .rightJoin('activity', 'activity', 'activity.id')
+      .where({user_id: userId})
+      .orderBy([{column: 'accepted_count', order: 'desc'}, {column: 'name'}])
+      .limit(10);
+  },
 
-    insertAcceptedRejectedRow(db, newRow) {
-        return db
-                .insert(newRow)
-                .into('accepted_rejected')
-                .returning('*')
-                .then(([row]) => row)
-    },
+  insertAcceptedRejectedRow(db, newRow) {
+    return db
+      .insert(newRow)
+      .into('accepted_rejected')
+      .returning('*')
+      .then(([row]) => row);
+  },
 
-    getUserActivityAcceptance(db, userId, activityId) {
-        return db
-                .select('*')
-                .from('accepted_rejected')
-                .where({user_id: userId, activity: activityId})
-    },
+  getUserActivityAcceptance(db, userId, activityId) {
+    return db
+      .select('*')
+      .from('accepted_rejected')
+      .where({user_id: userId, activity: activityId});
+  },
 
-    updateAcceptedCount(db, activityId, userId, acceptedCount){
-        return db('accepted_rejected')
-                .where({activity: activityId, user_id: userId})
-                .first()
-                .update({accepted_count: acceptedCount})
-                .then(rows => {
-                    return rows[0];
-                  });
-    },
+  updateAcceptedCount(db, activityId, userId, acceptedCount){
+    return db('accepted_rejected')
+      .where({activity: activityId, user_id: userId})
+      .first()
+      .update({accepted_count: acceptedCount})
+      .then(rows => {
+        return rows[0];
+      });
+  },
  
 };
 
