10e6099f96a1aed0223621d3734c9e1411e020d3
Blade Boles <bladerboles@gmail.com>
2020-04-06 13:27:00 -0600

    Preparing for database deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL:process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/indecisio',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/indecisio_test', 
   JWT_SECRET: process.env.JWT_SECRET || 'My name is a weapon',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   API_KEY: process.env.API_KEY,
