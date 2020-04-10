04372bcf531134f21fd588cb8156a1803b4ec63e
Blade Boles <bladerboles@gmail.com>
2020-04-10 10:14:47 -0600

    - to _

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/indecisio',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/indecisio-test', 
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/indecisio_test', 
   JWT_SECRET: process.env.JWT_SECRET || 'My name is a weapon',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   API_KEY: process.env.API_KEY,
--- test/setup.js
+++ test/setup.js
@@ -6,7 +6,7 @@ process.env.JWT_EXPIRY = '3m';
 require('dotenv').config();
 
 process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
-  || "postgresql://dunder_mifflin@localhost/indecisio-test";
+  || "postgresql://dunder_mifflin@localhost/indecisio_test";
 
 const { expect } = require('chai');
 const supertest = require('supertest');
